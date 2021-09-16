78a17f8ad0b1fd0100baa59869361236a6790ec6
James Carlo C. Mayrina <jamescarlomayrina@gmail.com>
2021-09-16 19:44:46 +0800

    added gallery

--- achievements.php
+++ achievements.php
@@ -17,10 +17,50 @@
   ?>
     <!-- Main -->
     <main>
+    <section class="acv">
     <div class="achieve">
     <h1 class="header">Achievements</h1>
     <h2 class="sales">Closed Sales</h2>
+    <div class="container-fluid">
+    <div class="row">
+      <div class="column">
+        <img src="./images/CLOSED SALES/August 27, 2021.jpg" />
+        <img src="./images/CLOSED SALES/August 1, 2020.jpg" />
+        <img src="./images/CLOSED SALES/August 27, 2021-2.jpg" />
+        <img src="./images/CLOSED SALES/August 27, 2021-3.jpg" />
+        <img src="./images/CLOSED SALES/June 2, 2021-2.jpg" />
+        <img src="./images/CLOSED SALES/June 25, 2020-2.jpg">
+        <img src="./images/CLOSED SALES/March 12, 2021-2.jpg">
+      </div>
+      <div class="column">
+        <img src="./images/CLOSED SALES/December 24, 2020-2.jpg">
+        <img src="./images/CLOSED SALES/December 21, 2020.jpg">
+        <img src="./images/CLOSED SALES/July 14, 2020-2.jpg" />
+        <img src="./images/CLOSED SALES/July 14, 2020-3.jpg">
+        <img src="./images/CLOSED SALES/June 2, 2021-3.jpg">
+        <img src="./images/CLOSED SALES/March 12, 2021.jpg">
+        <img src="./images/CLOSED SALES/September 22, 2020-2.jpg">
+      </div>
+      <div class="column">
+        <img src="./images/CLOSED SALES/December 24, 2020.jpg" />
+        <img src="./images/CLOSED SALES/February 21, 2021.jpg" />
+        <img src="./images/CLOSED SALES/February 22, 2021.jpg">
+        <img src="./images/CLOSED SALES/January 27, 2021.jpg">
+        <img src="./images/CLOSED SALES/January 28, 2021-2.jpg">
+        <img src="./images/CLOSED SALES/May 25, 2021-2.jpg">
+        <img src="./images/CLOSED SALES/May 25, 2021.jpg">
+      </div>
+      <div class="column">
+        <img src="./images/CLOSED SALES/December 24, 2020-3.jpg">
+        <img src="./images/CLOSED SALES/July 14, 2020.jpg" />
+        <img src="./images/CLOSED SALES/July 21, 2020-2.jpg">
+        <img src="./images/CLOSED SALES/July 21, 2020.jpg">
+        <img src="./images/CLOSED SALES/July 23, 2021-2.jpg">
+        <img src="./images/CLOSED SALES/June 25, 2020.jpg">
+      </div>
     </div>
+  </div>
+    </div></section>
     <!-- Interested -->
     <?php
     include('Includes/_interested.php');

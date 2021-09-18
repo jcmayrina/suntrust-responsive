e64186798245282b5c7ae24b582dd616e42e5185
James Carlo C. Mayrina <jamescarlomayrina@gmail.com>
2021-09-18 17:17:41 +0800

    fixed admin and detailed show

--- admin.php
+++ admin.php
@@ -20,5 +20,54 @@
         $logout = new Usercontrl();
         $logout->logout();;
         ?>
+        <div class="email">
+        <h1 class="header">Emails</h1>
+        <?php 
+        $email = new Userview();
+        $emails = $email->getEmail();
+        ?>
+        <table class="conts">
+                <tr>
+                    <th>Email</th>
+                    <th>Date & Time</th>
+                </tr>
+        <?php 
+        foreach($emails as $item){
+            $date = new DateTime($item['date_time']);
+            $result = $date->format('F d, Y | g:i A');
+            echo "<tr>
+                    <td>".$item['email_add']."</td>".
+                    "<td>".$result."</td>"
+                    ."</tr>";}?>
+                </table>
+        </div>
+        <div class="contactinfo">
+        <h1 class="header">Contacts</h1>
+        <?php 
+        $contact = new Userview();
+        $contacts = $contact->getContact();
+        ?>
+            <table class="conts">
+                <tr>
+                    <th>Full Name</th>
+                    <th>Email</th>
+                    <th>Number</th>
+                    <th>Property</th>
+                    <th>Message</th>
+                    <th>Date & Time</th>
+                </tr>
+                <?php
+                foreach($contacts as $item){
+                echo "<tr>
+                    <td>".$item['fullname']."</td>".
+                    "<td>".$item['email']."</td>".
+                    "<td>".$item['number']."</td>".
+                    "<td>".$item['property']."</td>".
+                    "<td>".$item['message']."</td>".
+                    "<td>".$item['date_time']."</td>"
+                    ."</tr>";}?>
+            </table>
+        </div>
+        </div>
       </main>
   </body>
--- classes/userview.php
+++ classes/userview.php
@@ -19,5 +19,22 @@ class Userview extends Dbh{
         return $names;
     }
 
+    public function getEmail(){
+        $sql = "SELECT * FROM `email` ORDER BY date_time DESC";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute();
+        $names = $stmt->fetchAll();
+
+        return $names;
+    }
+    
+    public function getContact(){
+        $sql = $this->connect()->prepare("SELECT * FROM `contact` ORDER BY date_time DESC");
+        $sql->execute();
+        
+        $names = $sql->fetchAll();
+
+        return $names;
+    }
 }
 ?>
